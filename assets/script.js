cf80a8da680699617bc3274f8d4ed7701451ea2e
Jakob Knicley <knicleyjakob@gmail.com>
2021-02-11 22:24:25 -0500

    added dropdown to roll

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,26 @@
-console.log("hi")
+let classesButton = document.getElementById("classes-button");
+let classesDropdown = document.getElementById("class-dropdown");
+
+classesButton.addEventListener("click", function(event){
+  //let element = event.target;
+  //console.log(element);
+    let state = classesDropdown.getAttribute("data-state");
+    if (state === "hidden") {
+      classesDropdown.dataset.state = "visible";
+      classesDropdown.classList.add("show");
+      classesDropdown.classList.remove("hide");
+      console.log("this is hidden to visible")
+    } else {
+      classesDropdown.dataset.state = "hidden";
+      classesDropdown.classList.add("hide");
+      classesDropdown.classList.remove("show");
+      console.log("this is visible to hidden")
+    }
+  }
+)
+
+classesButton.addEventListener("blur", function() {
+  classesDropdown.dataset.state = "hidden";
+  classesDropdown.classList.add("hide");
+  classesDropdown.classList.remove("show");
+})
